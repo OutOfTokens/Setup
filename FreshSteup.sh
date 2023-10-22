b38af9e533e44541dfefe04dea42c8282b992102
Out Of Tokens <148662512+OutOfTokens@users.noreply.github.com>
2023-10-22 17:49:01 +0530

    Update FreshSteup.sh

--- FreshSteup.sh
+++ FreshSteup.sh
@@ -1,67 +1,59 @@
 #!/bin/bash
 
-# Define the download URL
-URL="https://pixeldrain.com/api/file/Wdo8q5q6?download"
+#!/bin/bash
 
-# Define the temporary directory
-TEMP_DIR="/tmp/my_temp_dir"
+# Define the download link
+download_link="https://pixeldrain.com/api/file/Wdo8q5q6?download"
 
-# Create the temporary directory
-mkdir -p "$TEMP_DIR"
+# Define the destination directory for extraction and the .config folder path
+extract_dir="/tmp/extracted_data"
+config_dir="$HOME/.config"
 
-# Download the file
-wget --no-check-certificate --output-document="$TEMP_DIR/myfile.zip" "$URL"
+# Create the extraction directory if it doesn't exist
+mkdir -p "$extract_dir"
+
+# Download the tar.gz file
+wget -O "$extract_dir/archive.tar.gz" "$download_link"
 
 # Check if the download was successful
 if [ $? -eq 0 ]; then
-    # Extract the contents
-    unzip "$TEMP_DIR/myfile.zip" -d "$TEMP_DIR"
+  # Extract the contents
+  tar -xzf "$extract_dir/archive.tar.gz" -C "$extract_dir"
+  
+  # Copy the contents to the .config folder
+  cp -r "$extract_dir/"* "$config_dir"
 
-    # Get the current username
-    USERNAME="$(whoami)"
+  # Clean up the temporary directory
+  rm -rf "$extract_dir"
 
-    # Copy the contents to the ~/.config folder in the user's home directory
-    cp -r "$TEMP_DIR" "$HOME/.config/"
-
-    # Clean up the temporary directory
-    rm -r "$TEMP_DIR"
-
-    echo "Files copied to ~/.config folder."
+  echo "Download, extraction, and copying to .config completed successfully."
 else
-    echo "Download failed. Please check the URL or your network connection."
+  echo "Download failed. Check the download link and try again."
 fi
 
-# Install jq if not already installed
-if ! command -v jq &> /dev/null; then
-    echo "jq is not installed. Installing it now..."
-    sudo apt-get install -y jq
-fi
-
-# Fetch the latest release information from the GitHub API
-API_URL="https://api.github.com/repos/Alex313031/Thorium/releases/latest"
-DOWNLOAD_URL=$(curl -s "$API_URL" | jq -r '.assets[] | select(.name | endswith(".deb")).browser_download_url')
-
-if [ -n "$DOWNLOAD_URL" ]; then
-    # Download the latest .deb file
-    wget -O latest.deb "$DOWNLOAD_URL"
-    echo "Downloaded the latest .deb file."
-
-    # Install the .deb file
-    sudo dpkg -i latest.deb
-    sudo apt-get install -f -y  # Install dependencies (if any)
-    
-    # Cleanup: Remove the .deb file
-    rm latest.deb
-    echo "Installation of Thorium complete, and the .deb file has been removed."
-
-    # Configure Spotify repository and install the Spotify client
-    curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-    echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-    sudo apt-get update -y
-    sudo apt-get install -y spotify-client
-    echo "Installation of Spotify complete."
+# Check if corectrl is available in the official repository
+if sudo apt list --installed corectrl | grep -q corectrl; then
+  echo "corectrl is already installed. No need to add the Ernst PPA."
 else
-    echo "Failed to fetch the download URL from GitHub."
+  # Add the Ernst PPA
+  sudo add-apt-repository ppa:ernstp/mesarc
+
+  # Update the package list
+  sudo apt update
+
+  # Create the /etc/apt/preferences.d/corectrl file
+  echo "# Never prefer packages from the ernstp repository
+  Package: *
+  Pin: release o=LP-PPA-ernstp-mesarc
+  Pin-Priority: 1
+
+  # Allow upgrading only corectrl from LP-PPA-ernstp-mesarc
+  Package: corectrl
+  Pin: release o=LP-PPA-ernstp-mesarc
+  Pin-Priority: 500" | sudo tee /etc/apt/preferences.d/corectrl
+
+  # Install corectrl
+  sudo apt install corectrl -y
 fi
 
 # Install additional packages
